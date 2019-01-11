2e32037bb34787f17a85741cee80f1ff1d10ea84
magnae2016 <magnae2016@gmail.com>
2019-01-11 23:22:42 +0900

    Add menus to the Toolbar
    Add required menu to each toolbar.

--- app/src/main/java/com/magnae/bcl/LocationFragment.java
+++ app/src/main/java/com/magnae/bcl/LocationFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,6 +23,12 @@ public class LocationFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +43,12 @@ public class LocationFragment extends Fragment {
         activity.getSupportActionBar().setTitle(R.string.title_location_on);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_location, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
 }
