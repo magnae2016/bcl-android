5d772e00fc2bf749b97688ef50abbab0a2a428df
magnae2016 <magnae2016@gmail.com>
2019-01-15 23:24:06 +0900

    Add maps based on Daum Maps
    With the Maps SDK for Android, apply Namsan location to Daum map.

--- app/src/main/java/com/magnae/bcl/LocationFragment.java
+++ app/src/main/java/com/magnae/bcl/LocationFragment.java
@@ -5,19 +5,27 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements MapView.OpenAPIKeyAuthenticationResultListener,
+        MapView.MapViewEventListener {
+
+    private static final String LOG_TAG = "MapViewDemoActivity";
 
     private Toolbar myToolbar;
+    private MapView mMapView;
 
     public LocationFragment() {
         // Required empty public constructor
@@ -42,6 +50,22 @@ public class LocationFragment extends Fragment {
         activity.setSupportActionBar(myToolbar);
         activity.getSupportActionBar().setTitle(R.string.title_location_on);
 
+        mMapView = new MapView(activity);
+
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map);
+        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(37.551186, 126.988227);
+        mMapView.setMapCenterPoint(mapPoint, true);
+
+        mapViewContainer.addView(mMapView);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("부천시립도서관");
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mMapView.addPOIItem(marker);
+
         return view;
     }
 
@@ -51,4 +75,54 @@ public class LocationFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        Log.i(LOG_TAG, "MapView had loaded. Now, MapView APIs could be called safely");
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapCenterPoint) {
+        MapPoint.GeoCoordinate mapPointGeo = mapCenterPoint.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onMapViewCenterPointMoved (%f,%f)", mapPointGeo.latitude, mapPointGeo.longitude));
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int i, String s) {
+        Log.i(LOG_TAG,	String.format("Open API Key Authentication Result : code=%d, message=%s", i, s));
+    }
 }
