4eb8cd5fc265b01ddb71329f20fd4a3de7b9e4ff
magnae2016 <magnae2016@gmail.com>
2019-01-16 21:10:40 +0900

    Add maps based on Naver Maps
    With the Maps SDK for Android, apply Libraries in Bucheon to Naver map.

--- app/src/main/java/com/magnae/bcl/LocationFragment.java
+++ app/src/main/java/com/magnae/bcl/LocationFragment.java
@@ -3,6 +3,7 @@ package com.magnae.bcl;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -11,13 +12,23 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Align;
+import com.naver.maps.map.overlay.Marker;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
     private Toolbar myToolbar;
+    private MapFragment mapFragment;
+
 
     public LocationFragment() {
         // Required empty public constructor
@@ -27,6 +38,7 @@ public class LocationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setRetainInstance(true);
         setHasOptionsMenu(true);
     }
 
@@ -42,6 +54,16 @@ public class LocationFragment extends Fragment {
         activity.setSupportActionBar(myToolbar);
         activity.getSupportActionBar().setTitle(R.string.title_location_on);
 
+        NaverMapOptions options = new NaverMapOptions()
+                .camera(new CameraPosition(new LatLng(37.503198, 126.775964), 11.5))
+                .mapType(NaverMap.MapType.Basic);
+
+        mapFragment = (MapFragment) activity.getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment = MapFragment.newInstance(options);
+        activity.getSupportFragmentManager().beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+
+        mapFragment.getMapAsync(this);
+
         return view;
     }
 
@@ -49,6 +71,46 @@ public class LocationFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_location, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+        Marker markerAA = new Marker();
+        markerAA.setPosition(new LatLng(37.490072, 126.744688));
+        markerAA.setCaptionAlign(Align.Bottom);
+        markerAA.setCaptionText("상동도서관");
+        markerAA.setIconPerspectiveEnabled(true);
+        markerAA.setMap(naverMap);
+
+        Marker markerAB = new Marker();
+        markerAB.setPosition(new LatLng(37.498934, 126.7981866));
+        markerAB.setCaptionAlign(Align.Bottom);
+        markerAB.setCaptionText("원미도서관");
+        markerAB.setIconPerspectiveEnabled(true);
+        markerAB.setMap(naverMap);
+
+        Marker markerAC = new Marker();
+        markerAC.setPosition(new LatLng(37.480248, 126.783911));
+        markerAC.setCaptionAlign(Align.Bottom);
+        markerAC.setCaptionText("심곡도서관");
+        markerAC.setIconPerspectiveEnabled(true);
+        markerAC.setMap(naverMap);
+
+        Marker markerAD = new Marker();
+        markerAD.setPosition(new LatLng(37.520542, 126.794618));
+        markerAD.setCaptionAlign(Align.Bottom);
+        markerAD.setCaptionText("북부도서관");
+        markerAD.setIconPerspectiveEnabled(true);
+        markerAD.setMap(naverMap);
+
+        Marker markerAE = new Marker();
+        markerAE.setPosition(new LatLng(37.508202, 126.773745));
+        markerAE.setCaptionAlign(Align.Bottom);
+        markerAE.setCaptionText("꿈도서관");
+        markerAE.setIconPerspectiveEnabled(true);
+        markerAE.setMap(naverMap);
     }
 
 }
