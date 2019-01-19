bab1b88c8e57bc5f20dbfe844a7d6d2aee77b2aa
magnae2016 <magnae2016@gmail.com>
2019-01-19 23:57:36 +0900

    Update Markers for library locations
    Create a marker by organizing Bucheon City
     Libraries.
    
    Set the symbol color of the library to the color of the marker.

--- app/src/main/java/com/magnae/bcl/LocationFragment.java
+++ app/src/main/java/com/magnae/bcl/LocationFragment.java
@@ -22,7 +22,9 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Align;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.util.MarkerIcons;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -89,6 +91,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         markerAA.setCaptionAlign(Align.Bottom);
         markerAA.setCaptionText("상동도서관");
         markerAA.setIconPerspectiveEnabled(true);
+        markerAA.setIcon(MarkerIcons.GREEN);
         markerAA.setMap(naverMap);
 
         Marker markerAB = new Marker();
@@ -96,6 +99,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         markerAB.setCaptionAlign(Align.Bottom);
         markerAB.setCaptionText("원미도서관");
         markerAB.setIconPerspectiveEnabled(true);
+        markerAB.setIcon(MarkerIcons.YELLOW);
         markerAB.setMap(naverMap);
 
         Marker markerAC = new Marker();
@@ -103,6 +107,7 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         markerAC.setCaptionAlign(Align.Bottom);
         markerAC.setCaptionText("심곡도서관");
         markerAC.setIconPerspectiveEnabled(true);
+        markerAC.setIcon(MarkerIcons.YELLOW);
         markerAC.setMap(naverMap);
 
         Marker markerAD = new Marker();
@@ -110,14 +115,73 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
         markerAD.setCaptionAlign(Align.Bottom);
         markerAD.setCaptionText("북부도서관");
         markerAD.setIconPerspectiveEnabled(true);
+        markerAD.setIcon(MarkerIcons.LIGHTBLUE);
         markerAD.setMap(naverMap);
 
         Marker markerAE = new Marker();
         markerAE.setPosition(new LatLng(37.508202, 126.773745));
         markerAE.setCaptionAlign(Align.Bottom);
-        markerAE.setCaptionText("꿈도서관");
+        markerAE.setCaptionText("꿈빛도서관");
         markerAE.setIconPerspectiveEnabled(true);
+        markerAE.setIcon(MarkerIcons.GREEN);
         markerAE.setMap(naverMap);
+
+        Marker markerAF = new Marker();
+        markerAF.setPosition(new LatLng(37.493931, 126.770068));
+        markerAF.setCaptionAlign(Align.Bottom);
+        markerAF.setCaptionText("책마루도서관");
+        markerAF.setIconPerspectiveEnabled(true);
+        markerAF.setIcon(MarkerIcons.GREEN);
+        markerAF.setMap(naverMap);
+
+        Marker markerAG = new Marker();
+        markerAG.setPosition(new LatLng(37.467791, 126.796517));
+        markerAG.setCaptionAlign(Align.Bottom);
+        markerAG.setCaptionText("한울빛도서관");
+        markerAG.setIconPerspectiveEnabled(true);
+        markerAG.setIcon(MarkerIcons.YELLOW);
+        markerAG.setMap(naverMap);
+
+        Marker markerAH = new Marker();
+        markerAH.setPosition(new LatLng(37.515386, 126.805572));
+        markerAH.setCaptionAlign(Align.Bottom);
+        markerAH.setCaptionText("꿈여울도서관");
+        markerAH.setIconPerspectiveEnabled(true);
+        markerAH.setIcon(MarkerIcons.LIGHTBLUE);
+        markerAH.setMap(naverMap);
+
+        Marker markerAI = new Marker();
+        markerAI.setPosition(new LatLng(37.4824868, 126.7664629));
+        markerAI.setCaptionAlign(Align.Bottom);
+        markerAI.setCaptionText("송내도서관");
+        markerAI.setIconPerspectiveEnabled(true);
+        markerAI.setIcon(MarkerIcons.GREEN);
+        markerAI.setMap(naverMap);
+
+        Marker markerAK = new Marker();
+        markerAK.setPosition(new LatLng(37.516485, 126.785366));
+        markerAK.setCaptionAlign(Align.Bottom);
+        markerAK.setCaptionText("도당도서관");
+        markerAK.setIconPerspectiveEnabled(true);
+        markerAK.setIcon(MarkerIcons.LIGHTBLUE);
+        markerAK.setMap(naverMap);
+
+        Marker markerAJ = new Marker();
+        markerAJ.setPosition(new LatLng(37.528180, 126.796453));
+        markerAJ.setCaptionAlign(Align.Bottom);
+        markerAJ.setCaptionText("오정도서관");
+        markerAJ.setIconPerspectiveEnabled(true);
+        markerAJ.setIcon(MarkerIcons.LIGHTBLUE);
+        markerAJ.setMap(naverMap);
+
+        Marker markerAL = new Marker();
+        markerAL.setPosition(new LatLng(37.49479, 126.754049));
+        markerAL.setCaptionAlign(Align.Bottom);
+        markerAL.setCaptionText("동화도서관");
+        markerAL.setIconPerspectiveEnabled(true);
+        markerAL.setIcon(MarkerIcons.GREEN);
+        markerAL.setMap(naverMap);
+
     }
 
 }
